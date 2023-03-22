66b96de93377f0ffafd62f9313f6148842a2405a
Douglas <dwbewiahn@gmail.com>
2023-03-22 12:33:48 +0000

    Add divisao de imagem e embaralhar

--- src/util/TabuleiroQuebraCabeca.java
+++ src/util/TabuleiroQuebraCabeca.java
@@ -7,20 +7,65 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class TabuleiroQuebraCabeca extends JPanel {
-  
+    private ArrayList<BufferedImage> pecas;
+    private ArrayList<BufferedImage> pecasOriginais;
     private int linhas;
     private int colunas;
+    private int larguraPreferida;
+    private int alturaPreferida;
 
     public TabuleiroQuebraCabeca(BufferedImage imagem, int linhas, int colunas) {
         this.linhas = linhas;
         this.colunas = colunas;
+        pecas = new ArrayList<>();
+        pecasOriginais = new ArrayList<>();
+        larguraPreferida = imagem.getWidth();
+        alturaPreferida = imagem.getHeight();
 
-}
-public void trocarPecas(int indice1, int indice2) {
-   
-}
+        // Divide a imagem em peças
+        int larguraPeca = imagem.getWidth() / colunas;
+        int alturaPeca = imagem.getHeight() / linhas;
 
-public boolean estaResolvido() {
-    return false;
-}
+        for (int i = 0; i < linhas; i++) {
+            for (int j = 0; j < colunas; j++) {
+                BufferedImage peca = imagem.getSubimage(j * larguraPeca, i * alturaPeca, larguraPeca, alturaPeca);
+                pecas.add(peca);
+                pecasOriginais.add(peca);
+            }
+        }
+
+        // Embaralha as peças
+        Collections.shuffle(pecas);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+    
+        int larguraPeca = getWidth() / colunas;
+        int alturaPeca = getHeight() / linhas;
+        g.setFont(new Font("Arial", Font.BOLD, 100));
+        g.setColor(Color.WHITE);
+    
+        for (int i = 0; i < linhas; i++) {
+            for (int j = 0; j < colunas; j++) {
+                int indice = i * colunas + j;
+                g.drawImage(pecas.get(indice), j * larguraPeca, i * alturaPeca, larguraPeca, alturaPeca, null);
+                g.drawString(Integer.toString(indice+1), j * larguraPeca + 30, i * alturaPeca + 100);
+            }
+        }
+    }
+    
+    public void trocarPecas(int indice1, int indice2) {
+        Collections.swap(pecas, indice1, indice2);
+        repaint();
+    }
+
+    public boolean estaResolvido() {
+        return pecas.equals(pecasOriginais);
+    }
+    @Override
+    public Dimension getPreferredSize() {
+        return new Dimension(larguraPreferida, alturaPreferida);
+    }
 }
